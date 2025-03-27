e3d4a072b09976bfad4d5298da9975035ef73f4f
Vladyslav <bluues1536@gmail.com>
2025-03-27 01:39:25 +0000

    Prebuild Fixes02

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,6 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Space portfolio",
   description: "This is my portfolio",
