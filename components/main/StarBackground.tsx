f40e972e90e3d9baa60cc2c87db2ef00b5bfca58
Vladyslav <bluues1536@gmail.com>
2025-03-27 01:45:26 +0000

    Prebuild Fixes04

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -1,24 +1,30 @@
 "use client";
 
 import React, { useState, useRef, Suspense } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
 // @ts-expect-error because
 import * as random from "maath/random/dist/maath-random.esm";
+import { Points as ThreePoints } from "three";
 
-const StarBackground = (props: any) => {
-  const ref: any = useRef(null);
+type StarBackgroundProps = ThreeElements["group"] & {};
+
+const StarBackground = (props: StarBackgroundProps) => {
+  const ref = useRef<ThreePoints>(null);
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
-  useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
+
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
   });
 
   return (
-    <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+    <group rotation={[0, 0, Math.PI / 4]} {...props}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled>
         <PointMaterial
           transparent
           color="#fff"
